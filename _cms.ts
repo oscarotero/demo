b09e2ad727e2e54b61a9c8e8ddd0ee7f7eb054fb
Oscar Otero <oom@oscarotero.com>
2025-10-11 13:10:03 +0200

    probando

--- _cms.ts
+++ _cms.ts
@@ -2,4 +2,12 @@ import cms from "top/_cms.ts";
 
 cms.git();
 
+cms.auth({
+  manolo: {
+    name: "Manolo",
+    password: "manolo",
+    email: "manolo@manolo.com",
+  }
+})
+
 export default cms;
